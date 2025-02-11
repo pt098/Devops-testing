8ca895339a3353b74b8bda9f1c1102aa97b935e1
Priyanshu Thakur <148887714+pt098@users.noreply.github.com>
2025-02-11 22:37:38 +0530

    Update FeaturedTutorials.js

--- src/FeaturedTutorials.js
+++ src/FeaturedTutorials.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Card, Image } from 'semantic-ui-react'; // Import necessary components
+import { Card, Image } from 'semantic-ui-react';
 
 const tutorials = [
-  { title: 'JavaScript Tutorials', desc: 'Description- Learn A to Z JavaScript', author: 'John Smith', rating: 4, image: 'https://www.tutorialrepublic.com/lib/images/javascript-illustration.png' },
-  { title: 'React Understanding', desc: 'Description- Learn React in 10 hours', author: 'Priyanshu Thakur', rating: 5, image: 'https://miro.medium.com/v2/resize:fit:2000/1*z0teiQBZw9e0iPWlD47jTw.png' },
-  { title: 'ExpressJS Guide', desc: 'Description- Learn ExpressJS', author: 'Emilie', rating: 4.9, image: 'https://media.licdn.com/dms/image/D5612AQHqYWqhiP2bWQ/article-cover_image-shrink_720_1280/0/1713769258709?e=2147483647&v=beta&t=CYKXVlO-lUdX90cXS1wyXNkh9bJ61z7lE4TwzDDvCw0' },
+  {
+    title: 'JavaScript Tutorials',
+    desc: 'Description- Learn A to Z JavaScript',
+    author: 'John Smith',
+    rating: 4,
+    image: 'https://www.tutorialrepublic.com/lib/images/javascript-illustration.png'
+  },
+  {
+    title: 'React Understanding',
+    desc: 'Description- Learn React in 10 hours',
+    author: 'Priyanshu Thakur',
+    rating: 5,
+    image: 'https://miro.medium.com/v2/resize:fit:2000/1*z0teiQBZw9e0iPWlD47jTw.png'
+  },
+  {
+    title: 'ExpressJS Guide',
+    desc: 'Description- Learn ExpressJS',
+    author: 'Emilie',
+    rating: 4.9,
+    image: 'https://media.licdn.com/dms/image/D5612AQHqYWqhiP2bWQ/article-cover_image-shrink_720_1280/0/1713769258709?e=2147483647&v=beta&t=CYKXVlO-lUdX90cXS1wyXNkh9bJ61z7lE4TwzDDvCw0'
+  },
 ];
 
 function FeaturedTutorials() {
@@ -18,7 +36,10 @@ function FeaturedTutorials() {
             <Card.Content>
               <Card.Header>{tutorial.title}</Card.Header>
               <Card.Description>{tutorial.desc}</Card.Description>
-              <Card.Meta>⭐ {tutorial.rating} by {tutorial.author}</Card.Meta>
+              <Card.Meta>
+                {/* Wrap the emoji in a span with an accessible label */}
+                <span aria-label="star">⭐</span> {tutorial.rating} by {tutorial.author}
+              </Card.Meta>
             </Card.Content>
           </Card>
         ))}
