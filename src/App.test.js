ce2845ee24aef6d77931845d008fe73fcc4a8690
Priyanshu Thakur <148887714+pt098@users.noreply.github.com>
2025-02-12 13:30:15 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders welcome message', () => {
+test('renders header with DEV@Deakin', () => {
   render(<App />);
+  // Check for "DEV@Deakin" in the document.
   const headerElement = screen.getByText(/DEV@Deakin/i);
   expect(headerElement).toBeInTheDocument();
 });
